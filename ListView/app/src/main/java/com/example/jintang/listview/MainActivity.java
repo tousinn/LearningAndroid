d4bbead8003c1f6d9efd2e168e3e4daa9e889b39
Jin Tang <jin.tang@vmob.com>
2016-12-06 22:54:48 +1300

    Add the on click event

--- ListView/app/src/main/java/com/example/jintang/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/jintang/listview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.jintang.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter  = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,data);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(fruitAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }

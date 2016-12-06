b5241df100357df37c155065716f93c8128d0cc5
Jin Tang <jin.tang@vmob.com>
2016-12-06 22:25:06 +1300

    Improve the performance

--- ListView/app/src/main/java/com/example/jintang/listview/FruitAdapter.java
+++ ListView/app/src/main/java/com/example/jintang/listview/FruitAdapter.java
@@ -27,11 +27,26 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         Fruit fruit = getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resourceId,null);
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if (convertView == null){
+           view = LayoutInflater.from(getContext()).inflate(resourceId,null);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);
+        } else{
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+    class ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
